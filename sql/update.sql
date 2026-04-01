1c70d753505a5698c4fe876d99eb3fa478e4ac0f
Cristina Hernandez <crispallares7500@gmail.com>
2026-04-01 20:54:13 +0200

    Update SQL statements for email and age changes

--- sql/update.sql
+++ sql/update.sql
@@ -2,7 +2,4 @@
 UPDATE form SET email = 'lauratest@gmail.com' WHERE name ='laura';
 ---Cambiar edad 20 por 21--
 UPDATE form SET age = 21 WHERE age = 20;
----Eliminar todos los registros con edad 30---
-DELETE FROM form WHERE age = 30;
----Eliminar todos los registros con nombre pedro---
-DELETE FROM form WHERE name = 'pedro';
+
